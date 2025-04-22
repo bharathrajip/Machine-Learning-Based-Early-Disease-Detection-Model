937caf71557c298c5564856ab29daa8235f40c92
bharathrajip <bharathrajibs@gmail.com>
2025-04-22 15:31:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ model = joblib.load('disease_prediction_model.pkl')
 st.title("AI Disease Prediction App")
 
 # Feature list
-features = ['Age', 'Gender', 'Blood_Pressure', 'Heart_Rate', 'Cholesterol',
-            'Glucose', 'Smoking', 'Alcohol', 'Physical_Activity']
+
 feature_names = ['feature1', 'feature2']
 # Create input form
 user_input = {}
